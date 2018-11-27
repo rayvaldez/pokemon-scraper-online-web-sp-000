7289a5622b5d36a1afc855b6c275db04a016d037
Ray Valdez <ray_valdez@hotmail.co.uk>
2018-11-27 21:52:57 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -8,7 +8,7 @@ class Pokemon
     @db = db
   end
 
-  def self.save(id, name, type)
-    @db.execute("INSERT INTO pokemon (id, name, type) VALUES (?, ?, ?)", id, name, type)
+  def self.save(id, name, @db)
+    db.execute("INSERT INTO pokemon (id, name, @db) VALUES (?, ?, ?)", id, name, @db)
   end
 end
